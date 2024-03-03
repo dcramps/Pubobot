7b0f39af15b2a5b958ed1bb9af646b3ad105730c
Aldehir Rojas <hello@alde.dev>
2024-03-03 03:13:10 -0600

    Add support for custom expressions and fix matcher (#23)

--- test/scenario/matcher.py
+++ test/scenario/matcher.py
@@ -33,12 +33,19 @@ def compile_simple_expression(expr: str) -> re.Pattern:
     "8"
     """
     pattern = StringIO()
-    capture_pattern = re.compile(r"{\s*(?P<name>[a-zA-Z0-9_][a-zA-Z0-9_-]*)\s*}")
+    capture_pattern = re.compile(
+        r"{\s*(?P<name>[a-zA-Z0-9_][a-zA-Z0-9_-]*)\s*(:(?P<expr>[^}]+))?}"
+    )
 
     pos = 0
     for match in capture_pattern.finditer(expr):
         name = match["name"]
-        capture_group = f"(?P<{name}>.*?)"
+
+        pattern_expr = match["expr"]
+        if pattern_expr is None:
+            pattern_expr = ".*?"
+
+        capture_group = f"(?P<{name}>{pattern_expr})"
 
         pattern.write(re.escape(expr[pos : match.start()]))
         pattern.write(capture_group)
@@ -113,14 +120,13 @@ class PickStageMatcher:
         ":{alpha_emote}: {alpha_team}\n"
         ":{beta_emote}: {beta_team}\n\n"
         "__Unpicked__:\n"
-        "{unpicked}"
+        r"{unpicked:(\d+\. \S+(, )?)+\b}"
     )
 
     _start_footer = compile_simple_expression("\r\n<@{turn_capt_id}> picks first!")
     _turn_footer = compile_simple_expression("\n<@{turn_capt_id}>'s turn to pick!")
 
-    _picked = compile_simple_expression("{name}")
-    _unpicked = compile_simple_expression("{num}. {name}")
+    _unpicked = compile_simple_expression(r"{num:\d+}. {name:\S+}")
 
     _ready = compile_simple_expression(
         "**TEAMS READY - Match {match_id}**\r\n\r\n"
@@ -174,15 +180,7 @@ class PickStageMatcher:
         return factory(fields, alpha_team, beta_team, unpicked)
 
     def _match_picked(self, text: str) -> Iterable[str]:
-        picked = []
-        for match in self._picked.finditer(text):
-            if not match:
-                break
-
-            name = match.group("name")
-            picked.append(name)
-
-        return picked
+        return text.split(", ")
 
     def _match_unpicked(self, text: str) -> Iterable[Tuple[int, str]]:
         unpicked = []
