d2dbcd00d6d908851606a83ba01b9b00aafad23d
dc <code@dc.wtf>
2024-03-03 10:42:16 -0800

    weird format output but ok

--- test/scenario/test_who.py
+++ test/scenario/test_who.py
@@ -39,7 +39,9 @@ async def test_who(pbot, pickup_factory):
     for player in ctf_players[:-1]:
         await pbot.send_message("!j ctf", player)
 
-    for _ in range(18): # 1 message per join, 1 message per "only 1 player needed" message
+    for _ in range(
+        18
+    ):  # 1 message per join, 1 message per "only 1 player needed" message
         await pbot.get_message()
 
     await pbot.send_message("!who", pbot.admin)
